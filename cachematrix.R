80207671ace9dacc18de31133a20c1e0cedc6fca
Ted Mendum <tmen2712@verizon.net>
2015-06-14 17:36:48 -0400

    a small change for testing git

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# A small change for testing git
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
